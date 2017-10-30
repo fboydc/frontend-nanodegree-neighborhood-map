14b8bffbbd9d7750b03df8d9923549acb69215c7
Felipe <fboydc@gmail.com>
2017-10-29 22:04:32 -0400

    Latesti Version

--- app/js/main.js
+++ app/js/main.js
@@ -1,66 +1,205 @@
-var neighborhoodData = [
-	{
-		name: "Exton",
-		state: "PA",
-		zipcodes: [19341, 19353],
-		areacodes: [610, 484],
-		latlong: [40.0326, 75.6275]
-	}
-]
+var locationData = {addressline: "708 N Whitford Rd, 191341, Exton, PA"};
 
 
 
 
-var Neighborhood = function(name, state, zipcodes, areacodes, latlong){
-	this.name = name;
-	this.state = state;
-	this.zipcodes = zipcodes;
-	this.areacodes = areacodes;
-	this.latlong = latlong;
+var Location = function(addressline){
+	this.addressline = addressline;
+	this.facilityLists = ko.observableArray([]);
+	this.currentFacilityList = ko.observable();
+	this.init();
+}
+
+
+var FacilityList = function(type, key){
+	this.type = type;
+	this.key = key;
 	this.facilities = ko.observableArray([]);
-	this.currentFacility = ko.observable();
 }
 
 
-var FacilityType = function(name){
-	this.name = name;
-	this.locations = ko.observableArray([]);
+var Facility = function(){
+
 }
 
-
-Neighborhood.prototype.init = function(){
-	this.facilities.push(new FacilityType('Hospitals'));
-	this.facilities.push(new FacilityType('Fitness/gym'));
-	this.facilities.push(new FacilityType('Food'));
-	this.facilities.push(new FacilityType('Golf'));
-	this.facilities.push(new FacilityType('Mall'));
-	this.facilities.push(new FacilityType('Schools'));
-	this.facilities.push(new FacilityType('Tennis'));
-	this.facilities.push(new FacilityType('Public Transit'));
-	this.facilities.push(new FacilityType('Night Life'));
-	this.facilities.push(new FacilityType('Supermarkets'));
-	this.facilities.push(new FacilityType('Mailing Services'));
+Location.prototype.init = function(){
+	this.facilityLists.push(new FacilityList('Hospitals', 'hospital'));
+	this.facilityLists.push(new FacilityList('Fitness/gym/sports', 'gym'));
+	this.facilityLists.push(new FacilityList('Food', 'restaurant'));
+	this.facilityLists.push(new FacilityList('Mall', 'shopping_mall'));
+	this.facilityLists.push(new FacilityList('Schools', 'school'));
+	this.facilityLists.push(new FacilityList('Public Transit', 'transit_station'));
+	this.facilityLists.push(new FacilityList('Night Life', 'night_club'));
+	this.facilityLists.push(new FacilityList('Groceries', 'convenience_store'));
+	this.facilityLists.push(new FacilityList('Mailing Services', 'post_office'));
 }
+
 
 
 var ViewModel = function(){
 	var self = this;
-	self.neighborhoodList = ko.observableArray([]);
-	self.currentNeighborhood = ko.observable();
+	this.location = ko.observable();
+	this.currentLocation = ko.observable();
+	this.currentLocation(new Location(locationData.addressline));
+	this.facilityMarkers = [];
+	this.locationMarker = null;
 
 
-	neighborhoodData.forEach(function(item){
-		var entry = new Neighborhood(item.name, item.state, item.zipcodes, item.areacodes, item.latlong);
-		entry.init();
-		self.neighborhoodList.push(entry);
-	});
+	this.emptyFieldsError = function(input){
 
-	self.currentNeighborhood(self.neighborhoodList()[0]);
+		input.style.border = "1px solid red";
+		var message_element = document.getElementById('messages');
+		message_element.innerHTML = "Error: address line field empty*";
+		message_element.style.color = "red";
+
+	}
+
+	this.resetAddressInput = function(input){
+		 input.value = '';
+		 input.style.border = "none";
+		 document.getElementById('messages').innerHTML='';
+	}
+
+	this.changeMap = function(result){
+		this.map.setCenter(result.geometry.location);
+		this.map.setZoom(15);
+		this.currentLocation(new Location(result.formatted_address));
+		this.currentLocation().latlong = result.geometry.location;
+		vmodel.createLocationMarker(vmodel.map);	
+		this.searchFacilities();
+	}
+
+	this.searchFacilities = function(){
+		this.bounds = new google.maps.LatLngBounds();
+		for(var i=0; i<this.currentLocation().facilityLists().length; i++){
+			this.currentList = this.currentLocation().facilityLists()[i];
+			this.service.nearbySearch({
+				location: this.currentLocation().latlong,
+				radius: 4828.03,
+				type: this.currentLocation().facilityLists()[i].key
+			}, this.facilitiesCallback);
+
+		}
+
+	}
+
+	this.facilitiesCallback = function(results, status){
+		console.log(self.currentList);
+		if(status === google.maps.places.PlacesServiceStatus.OK){
+			for(var j=0; j<results.length; j++){
+				self.createFacilityMarker(results[j]);
+			}
+		}
 
 
 
+	}
+
+	this.createFacilityMarker = function(place){
+		var location = place.geometry.location;
+		var marker = new google.maps.Marker({
+			map: vmodel.map,
+			icon: this.getIconData(place),
+			title: place.name,
+			position: location,
+			id: place.place_id
+		});
+
+
+		this.facilityMarkers.push(marker);
+
+		 if (place.geometry.viewport) {
+            this.bounds.union(place.geometry.viewport);
+          } else {
+            this.bounds.extend(location);
+          }
+          	//THIS IS NOT EFFICIENT AT THIS LOCATION
+           this.map.fitBounds(this.bounds);
+	}
+
+	this.removeFaciltyMarker = function(){
+		for(var i=0; i<this.facilityMarkers.length; i++){
+			this.facilityMarkers[i].setMap(null);
+		}
+
+		this.facilityMarkers = [];
+	}
+
+
+	this.createLocationMarker = function(map){
+		if(this.locationMarker)
+			this.locationMarker.setMap(null);
+
+		var marker = new google.maps.Marker({
+			map: map,
+			title: this.currentLocation().addressline,
+			position: this.currentLocation().latlong
+		});
+
+		this.locationMarker = marker;
+
+	}
+
+
+	this.getIconData = function(place){
+		return {
+			url: place.icon,
+			size: new google.maps.Size(35, 35),
+            origin: new google.maps.Point(0, 0),
+            anchor: new google.maps.Point(15, 34),
+            scaledSize: new google.maps.Size(25, 25)
+		}
+	}
+
+	//----------------EVENT LISTENERS ---------------------------------//
+
+	this.changeLocation = function(){
+		var input = document.getElementById('addressline_input');
+		if(input.value){
+
+			this.geocoder.geocode({'address': input.value, 
+				componentRestrictions: {country:'US'}}, function(results, status){
+				if(status === 'OK'){
+					if(results.length > 0){
+						self.changeMap(results[0]);
+
+					}else{
+						alert('Error: no results were found for your provided address.');
+					}
+				}
+			});
+			this.resetAddressInput(input);
+		}else{
+			this.emptyFieldsError(input);
+		}
+
+	}
 
 
 }
 
-ko.applyBindings(new ViewModel());
+
+
+function loadMap(){
+
+	vmodel.geocoder = new google.maps.Geocoder();
+	vmodel.geocoder.geocode({'address':vmodel.currentLocation().addressline},
+		function(results, status){
+			if(status === 'OK'){
+				vmodel.currentLocation().latlong =  results[0].geometry.location;
+				vmodel.map = new google.maps.Map(document.getElementById('map_container'), {
+					center: vmodel.currentLocation().latlong,
+					zoom: 15
+				});
+
+				vmodel.service = new google.maps.places.PlacesService(vmodel.map);
+				vmodel.createLocationMarker(vmodel.map);
+				vmodel.searchFacilities();
+			}
+		});
+
+}
+
+
+vmodel = new ViewModel();
+ko.applyBindings(vmodel);
